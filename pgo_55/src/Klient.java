56131e91786f0a30cf2a6b5d52dae360aaf74976
s33345AL <s33345@pjwstk.edu.pl>
2025-04-03 12:51:24 +0200

    zadanie5

---
+++ pgo_55/src/Klient.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Klient {
+    private String imieę;
+    private String nazwisko;
+    private String email;
+    private ArrayList<Wydarzenie> listaRezerwacji = new ArrayList<>();
+}
---
+++ pgo_55/src/Wydarzenie.java
@@ -0,0 +1,8 @@
+public class Wydarzenie {
+    private String nazwa;
+    private String data;
+    private String miejsce;
+    private int maxLiczbaMiejsc = 100;
+    private int dostępneMiejsca = 0;
+    private double cena;
+}
